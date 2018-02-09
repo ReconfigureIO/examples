11fe92db3918b7222a23fef7bc02b7c5d8784321
Max Siegieda <maxsiegieda@gmail.com>
2018-02-09 17:16:46 +0000

    move glide files under scripts dir

--- scripts/update-vendor.sh
+++ scripts/update-vendor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -maxdepth 1 -mindepth 1 -type d -not -path '*/\.*' -not -path 'scripts' | xargs -I{} bash -c "cd {} && cp ../glide.lock . && glide --yaml ../glide.yaml i && rm glide.lock && git add -A vendor"
+find . -maxdepth 1 -mindepth 1 -type d -not -path '*/\.*' -not -path './scripts' | xargs -I{} bash -c "cd {} && cp ../scripts/files/glide.lock . && glide --yaml ../scripts/files/glide.yaml i && rm glide.lock && git add -A vendor"
