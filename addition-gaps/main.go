62f7bd09df2c4ce8d8641512b8373c94d5b2c788
Rosie Yohannan <rosie.yohannan@reconfigure.io>
2018-02-22 22:24:05 +0000

    update FPGA code

--- addition-gaps/main.go
+++ addition-gaps/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	// Import the entire framework for interracting with SDAccel from Go (including bundled verilog)
+	//  Import the entire framework for interracting with SDAccel from Go (including bundled verilog)
 	_ "github.com/ReconfigureIO/sdaccel"
 
+	// Use the new AXI protocol package for interracting with memory
 	aximemory "github.com/ReconfigureIO/sdaccel/axi/memory"
 	axiprotocol "github.com/ReconfigureIO/sdaccel/axi/protocol"
 )
+
+// function to add two uint32s
+func Add(a uint32, b uint32) uint32 {
+	return a + b
+}
 
 func Top(
 	// The first set of arguments to this function can be any number
@@ -15,7 +21,7 @@ func Top(
 	// For this example, we have 3 arguments: two operands to add
 	// together and an address in shared memory where the FPGA will
 	// store the output.
-	// YOUR CODE: declare the first operand here
+  // YOUR CODE: declare the first operand here
 	// YOUR CODE: declare the second operand here
 	// YOUR CODE: declare the memory address for the FPGA to store the result
 
@@ -31,7 +37,7 @@ func Top(
 	go axiprotocol.ReadDisable(memReadAddr, memReadData)
 
 	// Add the two input integers together
-	// YOUR CODE: Perform the addition here
+	// YOUR CODE: Perform the addition here using the Add function
 
 	// Write the result of the addition to the shared memory address provided by the host
 	aximemory.WriteUInt32(
